b9c640a78ba16d2508520c84cfbd24e59086818d
David Obodo <obododavidsop@gmail.com>
2022-09-10 00:02:36 +0100

    Added betty coding style and removed trailing white space

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -12,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("%d is positive\n", n);	
+		printf("%d is positive\n", n);
 	if (n == 0)
 		printf("%d is zero\n", n);
 	if (n < 0)
