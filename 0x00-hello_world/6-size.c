229094a0c6ba11c5d664dfb9b1b230e046aecf8f
David Obodo <obododavidsop@gmail.com>
2022-09-09 04:06:25 +0100

    Updated 6-size: Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longInt;
+	long long int longLongInt;
+	float floatType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longInt));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongInt));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}
