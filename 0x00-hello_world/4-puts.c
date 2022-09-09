8ba2981e279f3fc98f0818cacf170b695cd09ba9
David Obodo <obododavidsop@gmail.com>
2022-09-09 03:30:07 +0100

    Using puts to output c command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * function: puts
+ * main: main
+ *
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
