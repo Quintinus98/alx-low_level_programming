4e88939b406ad2d74c9f1a7dfd68fbc705899b0d
David Obodo <obododavidsop@gmail.com>
2022-09-10 02:44:36 +0100

    Updated 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+	int m = '0';
+
+	while (n < '9')
+	{
+		while (m <= '9')
+		{
+			if (n == m)
+			{
+				m++;
+				continue;
+			}
+			putchar(n);
+			putchar(m);
+			putchar(',');
+			putchar(' ');
+			m++;
+		}
+		n++;
+		m = n+1;
+	}
+	putchar('\n');
+	return (0);
+}
