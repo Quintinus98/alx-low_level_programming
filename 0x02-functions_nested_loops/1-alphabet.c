9735752a44dec23e914405bea64e55e6629f9a5d
David Obodo <obododavidsop@gmail.com>
2022-09-14 03:10:26 +0100

    Betty coding style update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,10 @@
  */
 void print_alphabet(void)
 {
-        char c;
-        for (c = 'a'; c<='z'; ++c)
-        {
-                _putchar(c);
-        }
-        _putchar('\n');
-	return;
+	char c;
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+	       	_putchar(c);
+	}
+	_putchar('\n');
 }
-
