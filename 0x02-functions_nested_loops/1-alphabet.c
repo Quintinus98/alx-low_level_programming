715fdefd37ebc3f67155c9d0d4572c3abb7b0641
David Obodo <obododavidsop@gmail.com>
2022-09-14 02:55:21 +0100

    Test file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,19 +5,20 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
-
-void print_alphabet(void)
-{
-	char c;
-	for (c = 'a'; c<='z'; ++c)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
-}
 int main(void)
 {
 	print_alphabet();
 	return (0);
 }
+
+void print_alphabet(void)
+{
+        char c;
+        for (c = 'a'; c<='z'; ++c)
+        {
+                _putchar(c);
+        }
+        _putchar('\n');
+	return;
+}
+
