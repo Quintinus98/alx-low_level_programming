f2ff93b7f3c2c3689dcf1fadd6c16bf4ff0dd69a
David Obodo <obododavidsop@gmail.com>
2022-09-14 04:30:32 +0100

    Updated 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: Number value
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	int retVal;
+
+	if (n < 0)
+	{
+		retVal = n * (-1);
+	}
+	else
+	{
+		retVal = n;
+	}
+	return (retVal);
+}
