f95fce912263df8763dc2f4e43936c42d03237b9
David Obodo <obododavidsop@gmail.com>
2022-09-16 16:04:41 +0100

    Function multiplies 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Checks for uppercase character
+ * @a: Integer variable
+ * @b: Integer variable
+ *
+ * Return: Multiplies a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
