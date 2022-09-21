ab3be3ab852a3afbd1e2a67d4ff307e18cf46fca
David Obodo <obododavidsop@gmail.com>
2022-09-21 01:40:53 +0100

    keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Generates random passwords
+ * Return: 0
+ */
+int main(void)
+{
+	int r;
+
+	r = rand();
+	// printf("%d\n", r);
+	return (r);
+}
