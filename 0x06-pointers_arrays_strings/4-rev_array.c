1a1ffcf58f85c3bf1bb6b860c9afb3977b942285
David Obodo <obododavidsop@gmail.com>
2022-09-22 02:13:46 +0100

    Updated betty styles

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * reverse_array
+ * reverse_array - Reverse array
  * @a: array
  * @n: number of elements in array a
  */
@@ -13,8 +13,8 @@ void reverse_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		tmp = a[i];
-		a[i] = a[n-1];
-		a[n-1] = tmp;
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
 
 		n--;
 	}
