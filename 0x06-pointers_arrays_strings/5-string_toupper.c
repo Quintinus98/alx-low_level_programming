ec5147eed6202b60b1561b715bdd9fb0eb582649
David Obodo <obododavidsop@gmail.com>
2022-09-22 04:36:09 +0100

    Update 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,9 +4,8 @@
  * string_toupper - Changes all lowercase letters to uppercase
  * @c: Character
  *
- * Return: 0
+ * Return: Always 0.
  */
-
 char *string_toupper(char *c)
 {
 	char sVal = 32;
@@ -14,9 +13,7 @@ char *string_toupper(char *c)
 	while (*c != '\0')
 	{
 		if (*c >= 97 && *c <= 122)
-		{
 			*c = *c - sVal;
-		}
 		c++;
 	}
 
