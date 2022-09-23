070b3e49489a545b299ac04ac44f83b076cc110f
David Obodo <obododavidsop@gmail.com>
2022-09-23 04:30:06 +0100

    Betty not found

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,17 +5,14 @@
  * print_number - Prints an integer
  * @n: number to print
  */
-
 void print_number(int n)
 {
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n *= -1;
 	}
 	if (n / 10)
-	{
 		print_number(n / 10);
-	}
 	_putchar(n % 10 + '0');
 }
