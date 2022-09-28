080450700898a85636217bf04bbe652cc1e7e776
David Obodo <obododavidsop@gmail.com>
2022-09-28 01:41:27 +0100

    string character updated

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - Locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *sAdd;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			sAdd = s;
+			break;
+		}
+		s++;
+	}
+
+	s = sAdd;
+	return (s);
+}
