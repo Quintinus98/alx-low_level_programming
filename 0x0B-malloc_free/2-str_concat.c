b4f18c8e475e18d3799a389972d1b0928c2c975b
Obodo David <obododavidsop@gmail.com>
2023-07-10 23:35:42 +0100

    Updated arg cond

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,6 +31,11 @@ char *str_concat(char *s1, char *s2)
 	char *ar;
 	int i, j, len, len1, len2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	len1 = xlen(s1) - 1;
 	len2 = xlen(s2) - 1;
 	len = len1 + len2;
