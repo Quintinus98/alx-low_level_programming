0c0d04319552b22c74ae0f69a7f5b3a2bd3b7f7a
Obodo David <obododavidsop@gmail.com>
2023-07-10 23:30:21 +0100

    Update on 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * xlen - Length of string
+ * @s: String
+ *
+ * Return: 0
+ */
+int xlen(char *s)
+{
+	unsigned int cnt = 0;
+
+	while (*s != '\0')
+	{
+		cnt++;
+		s++;
+	}
+
+	return (cnt + 1);
+}
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: first string argument.
+ * @s2: second string argument
+ *
+ * Return: a pointer to the array or null.
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *ar;
+	int i, j, len, len1, len2;
+
+	len1 = xlen(s1) - 1;
+	len2 = xlen(s2) - 1;
+	len = len1 + len2;
+	ar = malloc(len * sizeof(char));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		ar[i] = s1[i];
+	for (j = 0; j < len2; j++)
+		ar[len1 + j] = s2[j];
+
+	ar[len] = '\0';
+
+	return (ar);
+}
