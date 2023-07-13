e78e87a36e8669b6a840dfd6f2a58c18bca186e4
Obodo David <obododavidsop@gmail.com>
2023-07-13 12:03:07 +0100

    Added malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @int: an Integer
+ *
+ * Return: a pointer to the allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	char *ar;
+
+	ar = malloc(sizeof(int) * b);
+	if (ar == NULL)
+	{
+		printf("NULL\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	return (ar);
+}
