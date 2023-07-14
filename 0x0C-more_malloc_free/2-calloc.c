011dd7553945c92b8196ec9a9d2672f855bbd3fc
Obodo David <obododavidsop@gmail.com>
2023-07-14 06:49:28 +0100

    Added 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - allocates memory for an array, using malloc.
+ * @nmemb: number of elements in memory for an array
+ * @size: size of bytes
+ *
+ * Return: a pointer to the allocated memory
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ar;
+
+	if (!nmemb || !size)
+		return (NULL);
+
+	ar = malloc(size * nmemb);
+	if (!ar)
+		return (NULL);
+
+	return (ar);
+}
