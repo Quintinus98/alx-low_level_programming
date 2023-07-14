d68aca1d9481dff1d712bfd8637e42e68c88db1c
Obodo David <obododavidsop@gmail.com>
2023-07-14 13:01:29 +0100

    Update 5 Beta

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -53,9 +53,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 		ar = _memcpy(ar, ptr, new_size);
 
-	if (new_size < old_size)
-		ar = _memcpy(ar, ptr, old_size);
-
 	free(ptr);
 	return (ar);
 }
