bdb2e734ab4503ff310c9a7939d60fbe0570815f
Obodo David <obododavidsop@gmail.com>
2023-07-14 06:56:21 +0100

    beta update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,13 +12,17 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ar;
+	unsigned int i;
 
-	if (!nmemb || !size)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ar = malloc(size * nmemb);
 	if (!ar)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		ar[i] = 0;
+	
 
 	return (ar);
 }
