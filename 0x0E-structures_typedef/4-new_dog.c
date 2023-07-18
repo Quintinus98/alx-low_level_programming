f54024ebccab4beaee0d701b237def3e1f6dbd76
Obodo David <obododavidsop@gmail.com>
2023-07-18 01:22:46 +0100

    Update -free malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,9 +15,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	int len_name = 0, len_owner = 0, i;
 
-	/**
-	 * Getting the length of name and the length of owner
-	*/
 	while (name && name[len_name])
 		len_name++;
 	while (owner && owner[len_owner])
@@ -32,14 +29,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	*/
 	new_dog->name = malloc(sizeof(char) * (len_name + 1));
 	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
 		return (NULL);
+	}
 	for (i = 0; i < len_name; i++)
 		new_dog->name[i] = name[i];
 	new_dog->name[i] = '\0';
 
 	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
 	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
 		return (NULL);
+	}
 	for (i = 0; i < len_owner; i++)
 		new_dog->owner[i] = owner[i];
 	new_dog->owner[i] = '\0';
