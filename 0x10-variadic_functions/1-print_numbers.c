71e12d92d2df8ccffade276c8f91882dec34b51b
Obodo David <obododavidsop@gmail.com>
2023-07-21 21:01:23 +0100

    Update print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - sums all integers
+ * @separator: seperator
+ * @n: count of integers
+ *
+ * Return: Sum of variable arguments
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (n == 0)
+		exit(EXIT_SUCCESS);
+	if (separator == NULL)
+		separator = "";
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", va_arg(ap, int));
+			break;
+		}
+		printf("%d%s", va_arg(ap, int), separator);
+	}
+	va_end(ap);
+	exit(EXIT_SUCCESS);
+}
