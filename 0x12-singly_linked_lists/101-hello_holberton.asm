fe2c129ef4c85a85f4d7e1247002269bec03d4c7
Obodo David <obododavidsop@gmail.com>
2023-07-29 21:45:46 +0100

    updated 101-hello_holberton5

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
-          global    main
-          extern    printf
-main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+				global		_start
+				extern		printf
+_start:
+		mov		edi, fmt
+		xor		eax, eax
+		call	printf
+		mov		eax, 0
+		ret
+fmt: db `Hello, Holberton\n`,0
