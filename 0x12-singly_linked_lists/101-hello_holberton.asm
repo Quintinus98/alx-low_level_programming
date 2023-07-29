3af0f53e6a2b1f9920f5b4e93bc2fba921e177e4
Obodo David <obododavidsop@gmail.com>
2023-07-29 21:32:53 +0100

    updated 101-hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -17,14 +17,16 @@
 ;	Declare needed C  functions
 extern	printf		; the C function, to be called
 
-fmt:	db "Hello, Holberton\n", 10, 0	; The printf format, "\n",'0'
-	SECTION .text           ; Code section.
+fmt:	db "Hello, Holberton\n", 0	; The printf format, "\n",'0'
+
+SECTION .text           ; Code section.
 	global main		; the standard gcc entry point
+
 main:				; the program label for the entry point
   push    rbp		; set up stack frame
-	mov	rdi,fmt		; format for printf
+	mov	edi,fmt		; format for printf
 	mov	rax,0		; no xmm registers
   call    printf		; Call C function
 	pop	rbp		; restore stack
-	mov	rax,0		; normal, no error, return value
+	mov	eax,0		; normal, no error, return value
 	ret			; return
