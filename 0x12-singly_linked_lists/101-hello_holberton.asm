92cc7c6e2f00d4e2ae1bcb49e0ea2615f6d747b3
Obodo David <obododavidsop@gmail.com>
2023-07-29 21:40:16 +0100

    updated 101-hello_holberton2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,28 +1,13 @@
 ; 101-hello_holberton.asm    print a string using printf
-; Assemble:	  nasm -f elf64 101-hello_holberton.asm
-; Link:		  gcc -no-pie -std=gnu89 101-hello_holberton.o -o hello
-; Run:		  ./hello > hello.out
-; Output:	  cat hello_64.out
 
+				extern	printf		; the C function, to be called
+				global _start		; the standard gcc entry point
 
-; Equivalent C code
-; // hello.c
-; #include <stdio.h>
-; int main()
-; {
-;   printf("Hello, Holberton\n");
-;   return 0;
-; }
+_start:				; the program label for the entry point
+				mov	edi,	fmt		; format for printf
+				xor	eax,	eax
+				call	printf		; Call C function
+				mov	eax,	0		; normal, no error, return value
+				ret			; return
 
-;	Declare needed C  functions
-extern	printf		; the C function, to be called
-global main		; the standard gcc entry point
-
-main:				; the program label for the entry point
-	mov	edi,	fmt		; format for printf
-	xor	eax,	eax
-  call	printf		; Call C function
-	mov	eax,	0		; normal, no error, return value
-	ret			; return
-
-fmt:	db "Hello, Holberton\n", 0	; The printf format, "\n",'0'
+fmt:	db `Hello, Holberton\n`, 0	; The printf format, "\n",'0'
