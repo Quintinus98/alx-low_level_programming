56f4986173c0349a63d5f99b85bf312c42f77bb1
Obodo David <obododavidsop@gmail.com>
2023-07-29 21:42:35 +0100

    updated 101-hello_holberton3

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,12 @@
 ; 101-hello_holberton.asm    print a string using printf
 
-				extern	printf		; the C function, to be called
 				global _start		; the standard gcc entry point
-
+				extern	printf		; the C function, to be called
 _start:				; the program label for the entry point
-				mov	edi,	fmt		; format for printf
-				xor	eax,	eax
+				mov		edi, fmt		; format for printf
+				xor		eax, eax
 				call	printf		; Call C function
-				mov	eax,	0		; normal, no error, return value
+				mov		eax, 0		; normal, no error, return value
 				ret			; return
 
 fmt:	db `Hello, Holberton\n`, 0	; The printf format, "\n",'0'
