9d00bc861331dfebdde2ae80d9b08f4d3621b661
Obodo David <obododavidsop@gmail.com>
2023-07-31 18:30:23 +0100

    More singly linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Print list int
+ * @h: type listint_t
+ * Return: the number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *tmp = h;
+	unsigned int cnt = 0;
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		cnt++;
+	}
+
+	return (cnt);
+}
