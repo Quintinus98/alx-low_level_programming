5196d4e925a7207926eb4735b7832c05b4e492a5
Obodo David <obododavidsop@gmail.com>
2023-07-31 22:19:36 +0100

    Insert node updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode = malloc(sizeof(listint_t));
 	unsigned int cnt = 0;
 
-	if (!temp)
+	if (!temp || !newNode)
 		return (NULL);
 
 	newNode->n = n;
