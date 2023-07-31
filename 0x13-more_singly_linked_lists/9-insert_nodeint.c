99f5009399ae1f415eaf064ef57d745800a34542
Obodo David <obododavidsop@gmail.com>
2023-07-31 22:15:48 +0100

    Updated inserted

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (*head);
 	}
 
-	while (temp && idx > 0 && cnt > idx)
+	while (temp && cnt < idx)
 	{
 		if ((idx - 1) == (cnt))
 		{
