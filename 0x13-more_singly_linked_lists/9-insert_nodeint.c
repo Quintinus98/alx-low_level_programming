b69a3788e30b9c0e62e6e0d6fba5aa8ee817ae9c
Obodo David <obododavidsop@gmail.com>
2023-08-01 05:40:45 +0100

    Updated insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			newNode->next = temp->next;
 			temp->next = newNode;
-			return (temp);
+			return (newNode);
 		}
 		else
 			temp = (temp)->next;
