72a252e37aa6b1b575b5b50abb16fce08fa0901d
Obodo David <obododavidsop@gmail.com>
2023-08-06 15:59:13 +0100

    updated 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,25 +7,28 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 0x8000;
+	unsigned long int mask;
+	int i, showBit = 0;
 
-	if (n == 0)
+	/**
+	 * Example n = 98 -> 10011000 (Binary);
+	 * n >> i -> where i = 63;
+	 * mask: 0000 all zeros in 63 places.
+	 * when i = 7;
+	 * mask = 00000001;
+	 * we can then compare the 1 (last bit).
+	*/
+	for (i = 63; i >= 0; i--)
 	{
+		mask = n >> i;
+		if (mask & 1)
+		{
+			_putchar('1');
+			showBit++;
+		}
+		else if (showBit)
+			_putchar('0');
+	}
+	if (!showBit)
 		_putchar('0');
-		return;
-	}
-	if (n == 1)
-	{
-		_putchar('1');
-		return;
-	}
-
-	while (mask != 0)
-	{
-	if ((n & mask) == 0)
-		_putchar('0');
-	else
-		_putchar('1');
-	mask = mask >> 1;
-	}
 }
