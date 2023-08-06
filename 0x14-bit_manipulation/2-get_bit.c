912425e461d7cc686529e60368a688c96356cf81
Obodo David <obododavidsop@gmail.com>
2023-08-06 18:20:42 +0100

    Updated 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * count_bit - returns a count of the bits.
- * @n: decimal number.
- * Return: Bit count.
-*/
-int count_bit(unsigned long int n)
-{
-	int i, showBit = 0;
-	unsigned long int mask;
-
-	for (i = 63; i >= 0; i--)
-	{
-		mask = n >> i;
-		if (mask & 1)
-			showBit++;
-		else if (showBit)
-			showBit++;
-	}
-	return (showBit);
-}
-
-/**
  * get_bit - prints the binary representation of a number.
  * @n: decimal number
  * @index: index to return.
@@ -29,19 +8,12 @@ int count_bit(unsigned long int n)
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask, shift;
-	unsigned int cnt = count_bit(n);
+	unsigned long int shift, mask;
 
-	if (n == 0 && index == 0)
-		return (0);
-	if (index >= cnt)
+	if (index > 63)
 		return (-1);
 
 	shift = 1 << index;
 	mask = n & shift;
-
-	if (mask)
-		return (1);
-	else
-		return (0);
+	return (mask) ? 1 : 0;
 }
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -12,9 +12,32 @@ int main(void)
 
 	n = get_bit(1024, 10);
 	printf("%d\n", n);
-	n = get_bit(98, 1);
+	n = get_bit(0, 0);
 	printf("%d\n", n);
 	n = get_bit(1024, 0);
 	printf("%d\n", n);
 	return (0);
 }
+
+
+/**
+ * count_bit - returns a count of the bits.
+ * @n: decimal number.
+ * Return: Bit count.
+ *
+ * int count_bit(unsigned long int n)
+ * {
+ * 	int i, showBit = 0;
+ * 	unsigned long int mask;
+
+ * 	for (i = 63; i >= 0; i--)
+ * 	{
+ * 		mask = n >> i;
+ * 		if (mask & 1)
+ * 			showBit++;
+ * 		else if (showBit)
+ * 			showBit++;
+ * 	}
+ * 	return (showBit);
+ * }
+*/
