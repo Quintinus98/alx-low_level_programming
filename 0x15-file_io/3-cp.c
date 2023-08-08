f8162db37ee5d7c07e8f13b05e9429ce7263168c
Obodo David <obododavidsop@gmail.com>
2023-08-08 19:38:34 +0100

    Updated 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,21 +8,19 @@
 int main(int ac, char **av)
 {
 	int res, files[2];
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	files[0] = open(av[1], O_RDONLY);
 	if (!av[1] || files[0] == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	files[1] = creat(av[2], mode);
+	files[1] = creat(av[2], 0664);
 	if (files[1] == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[2]);
@@ -31,39 +29,33 @@ int main(int ac, char **av)
 	}
 
 	res = copy_from_to(files, av);
-	printf("-> %i)\n", res);
-	return (0);
+	return (res);
 }
 
 /**
  * copy_from_to -  copies the content of a file to another file.
- * @files: files to copy.
+ * @files: files to copy; copy from files[0] to files[1].
  * @av: argument list.
  * Return: a copy of file_from to file_to.
 */
 int copy_from_to(int files[], char **av)
 {
-	char *buffer;
-	int size = 1024, f_close, i;
-	ssize_t fw;
+	ssize_t cnt;
+	int i, f_close;
+	char buffer[BUFSIZ];
 
-	buffer = malloc(size);
-
-	if (read(files[0], buffer, size) != -1)
+	while ((cnt = read(files[0], buffer, sizeof(buffer))) != 0)
 	{
-		fw = write(files[1], buffer, size);
-		if (fw == -1)
+		if (write(files[1], buffer, cnt) != cnt)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			free(buffer);
+			close(files[0]);
 			exit(99);
 		}
 	}
-	else
+	if (cnt == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		free(buffer);
-		close(files[0]);
 		exit(98);
 	}
 
@@ -72,10 +64,10 @@ int copy_from_to(int files[], char **av)
 		f_close = close(files[i]);
 		if (f_close == -1)
 		{
-			dprintf(2, "Error: Can't close fd %d\n", -1);
-			free(buffer);
+			dprintf(2, "Error: Can't close fd %d\n", files[i]);
 			exit(100);
 		}
 	}
-	return (1);
+
+	return (0);
 }
