f94d457def54f48013280a92fca44c802cb3ba0f
Obodo David <obododavidsop@gmail.com>
2023-08-08 16:58:35 +0100

    Updated 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,13 +8,21 @@
 int main(int ac, char **av)
 {
 	int res;
+	char *file_from;
 
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	res = copy_from_to(av[1], av[2]);
+
+	file_from = av[1];
+	if (!file_from)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	res = copy_from_to(file_from, av[2]);
 	printf("-> %i)\n", res);
 	return (0);
 }
@@ -32,37 +40,31 @@ int copy_from_to(char *file_from, char *file_to)
 	ssize_t fr_from, fw;
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	if (!file_from)
+	fd = creat(file_to, mode);
+	fd_from = open(file_from, O_RDONLY);
+	buffer = malloc(size);
+
+	fr_from = read(fd_from, buffer, size);
+	if (fr_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fd = creat(file_to, mode);
-	fd_from = open(file_from, O_RDONLY);
 
-	buffer = malloc(size);
-	fr_from = read(fd_from, buffer, size);
-	if (fr_from == -1)
-		return (-1);
-
-	buffer[1024] = '\0';
 	fw = write(fd, buffer, size);
 	if (fd == -1 || fw == -1)
 	{
-		dprintf(fd, "Error: Can't write to %s", file_to);
+		dprintf(2, "Error: Can't write to %s", file_to);
+		free(buffer);
 		exit(99);
 	}
 
 	fd_close = close(fd);
-	if (fd_close == -1)
-	{
-		dprintf(fd, "Error: Can't close fd %d", -1);
-		exit(100);
-	}
 	fd_from_close = close(fd_from);
-	if (fd_from_close == -1)
+	if (fd_from_close == -1 || fd_close == -1)
 	{
-		dprintf(fd, "Error: Can't close fd %d", -1);
+		dprintf(2, "Error: Can't close fd %d", -1);
+		free(buffer);
 		exit(100);
 	}
 	return (1);
