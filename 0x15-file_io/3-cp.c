5156aeb9ee424c818c42b0caa6e7e0631af1168c
Obodo David <obododavidsop@gmail.com>
2023-08-08 18:37:41 +0100

    Major changes lol

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,8 +7,8 @@
  */
 int main(int ac, char **av)
 {
-	int res;
-	char *file_from;
+	int res, files[2];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
 	{
@@ -16,56 +16,66 @@ int main(int ac, char **av)
 		exit(97);
 	}
 
-	file_from = av[1];
-	if (!file_from)
+	files[0] = open(av[1], O_RDONLY);
+	if (!av[1] || files[0] == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	res = copy_from_to(file_from, av[2]);
+	files[1] = creat(av[2], mode);
+	if (files[1] == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[2]);
+		close(files[0]);
+		exit(98);
+	}
+
+	res = copy_from_to(files, av);
 	printf("-> %i)\n", res);
 	return (0);
 }
 
 /**
  * copy_from_to -  copies the content of a file to another file.
- * @file_from: Copy files from this.
- * @file_to: Copy files to this.
+ * @files: files to copy.
+ * @av: argument list.
  * Return: a copy of file_from to file_to.
 */
-int copy_from_to(char *file_from, char *file_to)
+int copy_from_to(int files[], char **av)
 {
 	char *buffer;
-	int fd, fd_close, fd_from, fd_from_close, size = 1024;
-	ssize_t fr_from, fw;
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int size = 1024, f_close, i;
+	ssize_t fw;
 
-	fd = creat(file_to, mode);
-	fd_from = open(file_from, O_RDONLY);
 	buffer = malloc(size);
 
-	fr_from = read(fd_from, buffer, size);
-	if (fr_from == -1)
+	if (read(files[0], buffer, size) != -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		fw = write(files[1], buffer, size);
+		if (fw == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			free(buffer);
+			exit(99);
+		}
+	}
+	else
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		free(buffer);
+		close(files[0]);
 		exit(98);
 	}
 
-	fw = write(fd, buffer, size);
-	if (fd == -1 || fw == -1)
+	for (i = 0; i < 2; i++)
 	{
-		dprintf(2, "Error: Can't write to %s", file_to);
-		free(buffer);
-		exit(99);
-	}
-
-	fd_close = close(fd);
-	fd_from_close = close(fd_from);
-	if (fd_from_close == -1 || fd_close == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d", -1);
-		free(buffer);
-		exit(100);
+		f_close = close(files[i]);
+		if (f_close == -1)
+		{
+			dprintf(2, "Error: Can't close fd %d\n", -1);
+			free(buffer);
+			exit(100);
+		}
 	}
 	return (1);
 }
