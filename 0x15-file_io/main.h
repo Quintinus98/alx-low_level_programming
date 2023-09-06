cf9024f15fa7f697c17637b70a7cb38911a6e71a
Obodo David <obododavidsop@gmail.com>
2023-09-06 07:03:34 +0100

    elf headers

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,9 +4,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
 
 #include <fcntl.h>
 #include <sys/stat.h>
+
+#define READ(f, var) fread(&(var), sizeof(var), 1, (f))
+typedef struct ElfHeader
+{
+	uint8_t  ident[16];
+	uint16_t e_type;
+	union
+	{
+		uint32_t entry32;
+		uint64_t entry64;
+	};
+} ElfHeader;
+void read_file(FILE *fin, ElfHeader *elf);
+void print_header(const ElfHeader *elf);
+const char *os_abi(uint8_t os);
+const char *file_type(uint16_t e_type);
+const char *get_data(uint8_t e_data);
+const char *get_class(uint8_t class);
+const char *get_ver(uint8_t ver);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
