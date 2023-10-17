d53c7bb8282f06561c176bf8f1d8b837e9ede80e
David <obododavidsop@gmail.com>
2023-10-17 04:11:36 +0100

    Update

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,66 @@
+/**
+ * add - Adds two numbers
+ * @a: Integer variable
+ * @b: Integer variable
+ *
+ * Return: Adds a and b.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts
+ * @a: Integer variable
+ * @b: Integer variable
+ *
+ * Return: Subtracts a and b.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two numbers
+ * @a: Integer variable
+ * @b: Integer variable
+ *
+ * Return: Multiplies a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides two numbers
+ * @a: Integer variable
+ * @b: Integer variable
+ *
+ * Return: Multiplies a and b.
+ */
+
+int div(int a, int b)
+{
+	if (b <= 0)
+		return (0);
+	return (a / b);
+}
+
+/**
+ * mod - Modulus two numbers
+ * @a: Integer variable
+ * @b: Integer variable
+ *
+ * Return: Multiplies a and b.
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
