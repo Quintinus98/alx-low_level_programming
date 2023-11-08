a7e98f43f6541ee0513f51477543b18643908568
David <obododavidsop@gmail.com>
2023-11-08 16:40:02 +0100

    Update

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -46,6 +46,6 @@ hash_node_t *update_at_idx(hash_node_t **head, int idx, hash_node_t *newNode);
 hash_node_t *create_node(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
-
+void hash_table_delete(hash_table_t *ht);
 
 #endif
